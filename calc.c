9b42a4ce7ebadfd3e82c06cc1ef033d52add88d9
Anish Mndhr <94118929+Anish-Mndhr@users.noreply.github.com>
2022-02-23 21:57:53 +0545

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,6 +1,6 @@
 /********************************************************************************************************
 Program Name: calculator.c
 Program Description: this is a calculator project.
-Program Author: <YOUR NAME HERE>
+Program Author: Anish Raj Manandhar
 ********************************************************************************************************/
 #include <stdio.h> //from experiment branch
